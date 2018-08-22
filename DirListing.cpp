9828d2bc0170c2f605b8c6af22da350c9b50432d
ravihooda155 <ravihooda155@gmail.com>
2018-08-22 22:33:47 +0530

    File Listing Implemented

---
+++ DirListing.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<dirent.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
+#include <pwd.h>
+#include <grp.h>
+using namespace std;
+
+int main()
+{
+	//clrscr();
+	struct stat buf;
+	struct tm *tym;
+	struct passwd *pws;
+	struct group *grp;
+	const char * months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+	DIR *dir;
+	int len;
+	string filename;
+	dirent *pdir;
+	int size;
+	dir=opendir(".");
+	cout<<"Name"<<std::string( 26, ' ' )<<"Size"<<std::string( 15, ' ' )<<"Ownership"<<std::string( 18, ' ' )<<"Group"<<std::string( 18, ' ' )<<"User"<<std::string( 17, ' ' )<<"Last Modified"<<endl;
+	while(readdir(dir))
+	{
+		pdir=readdir(dir);
+		filename=pdir->d_name;  
+     		stat(pdir->d_name,&buf);
+ 
+		len=filename.length();
+
+		//file name
+		cout<<pdir->d_name;
+		cout << std::string( 30-len, ' ' );
+
+		//file size
+		cout<<buf.st_size<<"B"<<std::string(2,'\t');
+
+		//ownership
+		 
+	        cout<<( (S_ISDIR(buf.st_mode)) ? "d" : "-");
+	        cout<<( (buf.st_mode & S_IRUSR) ? "r" : "-");
+	        cout<<( (buf.st_mode & S_IWUSR) ? "w" : "-");
+	        cout<<( (buf.st_mode & S_IXUSR) ? "x" : "-");
+	        cout<<( (buf.st_mode & S_IRGRP) ? "r" : "-");
+	        cout<<( (buf.st_mode & S_IWGRP) ? "w" : "-");
+	        cout<<( (buf.st_mode & S_IXGRP) ? "x" : "-");
+	        cout<<( (buf.st_mode & S_IROTH) ? "r" : "-");
+	        cout<<( (buf.st_mode & S_IWOTH) ? "w" : "-");
+		cout<<( (buf.st_mode & S_IXOTH) ? "x" : "-");
+		cout<<std::string(2,'\t');
+		
+		//group
+		grp = getgrgid(buf.st_gid);
+		if(strlen(grp->gr_name)<12)
+		{cout<<grp->gr_name;
+		cout<<std::string((12-strlen(grp->gr_name)),' ');
+		}
+		else if(strlen(grp->gr_name)>12)
+		{ string str=grp->gr_name;
+		str.resize(10);
+		cout<<str<<"..";
+		}
+		else
+		{cout<<grp->gr_name;
+		}
+		cout<<std::string(2,'\t');
+		
+		//owner
+		pws = getpwuid(buf.st_uid);
+		cout << pws->pw_name;
+		if(strlen(pws->pw_name)<12)
+		cout<<std::string((12-strlen(pws->pw_name)),' ');
+		cout<<std::string(2,'\t');
+
+		//file last modified
+		 tym = gmtime(&(buf.st_mtime));
+		  cout<<tym->tm_mday<<" "<<months[(tym->tm_mon)-1]<<" "<<tym->tm_hour<<":"<<tym->tm_min;
+		 
+		cout<<endl;
+	}
+	closedir(dir);
+	//getch();
+return 0;
+}
