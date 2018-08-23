f42d3c3fb58ba041208c9018b15e5d1d05ffb84b
ravihooda155 <ravihooda155@gmail.com>
2018-08-24 03:55:20 +0530

    Up down Navigation Added

--- DirListing.cpp
+++ DirListing.cpp
@@ -6,11 +6,31 @@
  #include <fcntl.h>
 #include <pwd.h>
 #include <grp.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include<string>
+#include<stdlib.h>
 using namespace std;
-
-int main()
+//struct termios saved_attributes;
+int getch(void)
 {
-	//clrscr();
+ int ch;
+ struct termios oldt;
+ struct termios newt;
+ tcgetattr(STDIN_FILENO, &oldt); 
+ newt = oldt;  
+ newt.c_lflag &= ~(ICANON | ECHO); 
+ tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+ ch = getchar(); 
+ tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+ return ch; 
+}
+void listing()
+{
+	
+	ofstream cout;
+        cout.open("/dev/pts/0",ios::out);
 	struct stat buf;
 	struct tm *tym;
 	struct passwd *pws;
@@ -22,9 +42,14 @@ int main()
 	dirent *pdir;
 	int size;
 	dir=opendir(".");
+	
+	
+	 cout<<"\u001b[31m";
 	cout<<"Name"<<std::string( 26, ' ' )<<"Size"<<std::string( 15, ' ' )<<"Ownership"<<std::string( 18, ' ' )<<"Group"<<std::string( 18, ' ' )<<"User"<<std::string( 17, ' ' )<<"Last Modified"<<endl;
+	cout<<"\u001b[0m";
+	cout<<"\u001b[33m";
 	while(readdir(dir))
-	{
+	{	
 		pdir=readdir(dir);
 		filename=pdir->d_name;  
      		stat(pdir->d_name,&buf);
@@ -40,7 +65,7 @@ int main()
 
 		//ownership
 		 
-	        cout<<( (S_ISDIR(buf.st_mode)) ? "d" : "-");
+	        cout<<( (S_ISDIR(buf.st_mode)) ? "d" : "f");
 	        cout<<( (buf.st_mode & S_IRUSR) ? "r" : "-");
 	        cout<<( (buf.st_mode & S_IWUSR) ? "w" : "-");
 	        cout<<( (buf.st_mode & S_IXUSR) ? "x" : "-");
@@ -78,10 +103,61 @@ int main()
 		//file last modified
 		 tym = gmtime(&(buf.st_mtime));
 		  cout<<tym->tm_mday<<" "<<months[(tym->tm_mon)-1]<<" "<<tym->tm_hour<<":"<<tym->tm_min;
-		 
+		
 		cout<<endl;
 	}
+	 cout<<"\u001b[0m";
+	cout<<"";
+	cout.close();
 	closedir(dir);
-	//getch();
+}
+int main(int argc, char **argv)
+{
+	int key1 = ' ';
+	 int key2 = ' ';
+	 int key3 = ' ';
+
+	string s;
+	//cout << "\033[2J\033[1;1H";
+	
+	listing();
+	while(1)
+	{
+		key1 = getch();
+		
+		 if (key1 == 27)
+		 {
+		  key2 = getch();
+		  key3= getch();
+		
+		 }
+
+		 if (key1== 27 && key2 == 91)
+		 {
+		  if(key3==65)
+		  {
+			
+			cout<<"\u001b[1A";
+			cout<<"\x1b[\x32 q";
+			
+		   }
+		    else if(key3==66)
+			{
+		     cout<<"\u001b[1B";
+			//cout<<"\e[?5l";	
+			}
+		  /* else if(key3==67)
+		     cout<<"\u001b[1C";
+		
+		  else if(key3==68)
+		     cout<<"\u001b[1D";	*/			
+		  
+		
+		  }
+		 
+		 
+	}
+	
+	
 return 0;
 }
